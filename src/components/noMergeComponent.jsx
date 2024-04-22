496a1fe902dd27e011ac53fab67f945f56d5fe72
Anton Zadorozniy <zadoroshniy_anton_2003@mail.ru>
2024-04-22 11:40:48 +0300

    test

---
+++ src/components/noMergeComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <div className="header">
+      <div>test1</div>
+      <div>test2</div>
+      <div>test3</div>
+      <div>test4</div>
+      <div>test5</div>
+    </div>
+  );
+};
+
+export default Header;
